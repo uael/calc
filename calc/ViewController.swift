3d3deb02aac8d09005b3e16c85639b7db9f172ce
LUCAS Abel <la511235@s202p12m.iutnice.unice.fr>
2017-02-28 12:12:11 +0100

    added basic operation

--- calc/ViewController.swift
+++ calc/ViewController.swift
@@ -1,25 +1,61 @@
-//
-//  ViewController.swift
-//  calc
-//
-//  Created by LUCAS Abel on 28/02/2017.
-//  Copyright © 2017 uael. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    @IBOutlet
+    weak var result: UILabel!
+    var isWriting = false
+    var numbers = Array<Double>()
+    var value: Double {
+        get {
+            return Double(result.text!)!
+        }
+        set {
+            result.text = "\(newValue)"
+            isWriting = false
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBOutlet weak var cells: UICollectionView!
+    
+    @IBAction
+    func nClick(_ sender: UIButton) {
+        if let chiffre = sender.currentTitle {
+            if isWriting {
+                result.text = result.text! + chiffre
+            } else {
+                result.text = chiffre
+                isWriting = true
+            }
+        }
     }
-
-
+    
+    @IBAction
+    func enter(_ sender: UIButton) {
+        numbers.append(value)
+        isWriting = false
+    }
+    
+    @IBAction
+    func operate(_ sender: UIButton) {
+        if numbers.count >= 2 {
+            switch sender.currentTitle! {
+            case "+":
+                value = numbers.popLast()! + numbers.popLast()!
+                enter(sender)
+            case "-":
+                value = numbers.popLast()! - numbers.popLast()!
+                enter(sender)
+            case "*":
+                value = numbers.popLast()! * numbers.popLast()!
+                enter(sender)
+            case "/":
+                value = numbers.popLast()! / numbers.popLast()!
+                enter(sender)
+            default:
+                break;
+            }
+        }
+    }
 }
+
 
